d125ea7571aa14cded5580a176b1910a6f0443ae
Zabir <121322821+zabzzz@users.noreply.github.com>
2025-10-19 03:06:22 -0400

    Profile page with student bool
    Student boolean simply entails whether a user is a student or not

--- user/models.py
+++ user/models.py
@@ -5,6 +5,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     info = models.TextField(blank=True, null=True)
+    is_student = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.user.username}'s profile"
