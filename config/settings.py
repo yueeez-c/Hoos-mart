ac1aba89963c3e7adfdef57a1c228d3509839097
Yuezhang Chen <mme3ar@virginia.edu>
2025-11-03 00:05:30 -0500

    fix bugs

--- config/settings.py
+++ config/settings.py
@@ -93,13 +93,17 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 # Database (Heroku auto configures DATABASE_URL)
-DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=False
-    )
-}
+if os.environ.get("DATABASE_URL"):  # running on Heroku
+    DATABASES = {
+        "default": dj_database_url.config(conn_max_age=600, ssl_require=True)
+    }
+else:  # local development uses SQLite
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
