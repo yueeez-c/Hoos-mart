3714c0b5eae8d0a362448de925a714117ef73b2a
Meredith <158351336+dadamere04@users.noreply.github.com>
2025-11-03 03:02:21 -0500

    Fix ASGI import order + Redis channel layer

--- config/asgi.py
+++ config/asgi.py
@@ -8,21 +8,19 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")  # <-- set FIRST
 
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()  # <-- initialize Django before importing your stuff
+
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack 
-import messaging.routing
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+from channels.auth import AuthMiddlewareStack
+import messaging.routing  # <-- safe to import now, after settings are ready
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket" : AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(messaging.routing.websocket_urlpatterns)
-        )
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(messaging.routing.websocket_urlpatterns)
     ),
 })
 
