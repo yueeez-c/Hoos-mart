8c96e9ef4397118d38c56c5db4ecc26de074f422
Yuezhang Chen <mme3ar@virginia.edu>
2025-11-23 05:20:12 -0500

    email verification

--- config/settings.py
+++ config/settings.py
@@ -198,8 +198,17 @@ ACCOUNT_FORMS = {
 }
 
 # if not DEBUG:
-EMAIL_BACKEND = "config.resend_backend.ResendEmailBackend"
-DEFAULT_FROM_EMAIL = "chyueez@gmail.com"
+# EMAIL_BACKEND = "config.resend_backend.ResendEmailBackend"
+# DEFAULT_FROM_EMAIL = "chyueez@gmail.com"
 # else:
 #     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER') # Your gmail address
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS') # Your App Password
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
